45d26b44aaed4d0542bec0d8580a160d46b5002f
Favour Ojiaku <ojiakufavour@gmail.com>
2021-07-21 04:32:51 +0100

    longest palindromic substring

--- breakAPalindrome.js
+++ breakAPalindrome.js
@@ -1,32 +1,61 @@
 /**
  * @param {string} palindrome
  * @return {string}
+ * @param isSameWord
  */
 
 const isPalindrome = require('./isPalindrome');
-
+const palindrome = "abccba";
+let left = 0, right = palindrome.length - 1, i=0;
 var breakPalindrome = function(palindrome) {
     let checkWord = palindrome;
     if (checkWord.length === 1){
         return palindrome = '';
     }
 
-    if(isPalindrome(palindrome)) {
-        let left = 0, right = palindrome.length - 1, i=0;
-           
+    if (isSameWord(palindrome, right)){
+        const ascii = palindrome.charCodeAt(0);
+        const diff = ascii + 1;
+        const changeChar = String.fromCharCode(diff)
+        palindrome = palindrome.replace(palindrome.charAt(right), changeChar);
+        return palindrome;
+    }
+
+    if(isPalindrome(palindrome)) {           
         while(left <= right) {
             if (palindrome[left] === palindrome[left+1]){
                 left++;
-            }
-            else {
-                palindrome = palindrome.replace(palindrome.charAt(left+1), palindrome.charAt(left));
+                console.log(palindrome[left])
                 break;
-            }  
+            }
+            // else {
+                
+                
+            // }  
         }
-      return palindrome;  
+        //return palindrome[1];
+      //return palindrome = palindrome.replace(palindrome.charAt(left+1), palindrome.charAt(left));;  
     }
     
 };
 
-const checkWord = 'madam';
-console.log(breakPalindrome(checkWord));
+// const isSameWord = (word, len) => {
+//     let sameWord;
+//     for (i = 0; i <= len; i++){
+//         if (word[i] === word[i+1]) {
+//             sameWord =+ word[i];
+//         }else {
+//             break;
+//         }
+//     }
+//     return true;
+// }
+
+const checkWord = 'aaca';
+
+// let len = checkWord.length -1;
+// //var n = isSameWord(checkWord, len)
+// len = checkWord.charCodeAt(checkWord[0]);
+// len=len +1;
+//console.log(breakPalindrome(palindrome));
+//console.log(checkWord.charAt(2));
---
+++ longestPalindromicSubstring.js
@@ -0,0 +1,32 @@
+const longestPalindrome = (word) => {
+    if (word === null || word.length < 1){
+        return "";
+    }
+    let start = 0, end = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        const len1 = expandAroundCenter(word, i, i);
+        const len2 = expandAroundCenter(word, i, i + 1);
+
+        const len = Math.max(len1, len2);
+        if (len > end - start) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+    return word.substring(start, end + 1);
+}
+
+
+const expandAroundCenter = (word, left, right) => {
+    let L = left, R = right;
+
+    while (L >= 0 && R < word.length && word.charAt(L) === word.charAt(R)) {
+        L--;
+        R++;
+    }
+    return R-L-1;
+}
+
+const checkWord = 'abacdfgdcaba';
+console.log(longestPalindrome(checkWord));
